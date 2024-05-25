8b069c3dfa6d82aa57ae843177db3a788b7636fa
Yanyu777 <71878399+Yanyu777@users.noreply.github.com>
2024-05-25 22:33:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+from scipy.stats import norm
 
 
 chat_id = 775883383 # Ваш chat ID, не меняйте название переменной
 
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    p_control = x_success / x_cnt
+    p_test = y_success / y_cnt
 
-def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:   
+    SE = np.sqrt((p_control * (1 - p_control) / x_cnt) + (p_test * (1 - p_test) / y_cnt))
+    Z = (p_test - p_control) / SE
+
     alpha = 0.02
-    
-    stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
-    
-    return p_value < alpha
+    z_critical = norm.ppf(1 - alpha / 2)
+
+    return abs(Z) > z_critical # Ваш ответ, True или False
